5e454680dd3c7908ce44fe5674644a4983721937
sotowang <sotowang@qq.com>
2018-07-12 12:33:11 +0800

    注解方式测试

--- src/com/soto/test/TestSpring.java
+++ src/com/soto/test/TestSpring.java
@@ -3,17 +3,23 @@ package com.soto.test;
 import com.soto.pojo.Category;
 import com.soto.pojo.Product;
 import com.soto.service.ProductService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestSpring {
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
+        @Autowired
+        Category c;
+    @Test
+    public void test() {
+        System.out.println(c.getName());
 
-        Product p = (Product) context.getBean("p");
+    }
 
-        ProductService s = (ProductService) context.getBean("s");
-        s.doSomeService();
-
-    };
 }
